d8d7e045d950693478e89965e0079351a99abfe1
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-15 15:02:00 -0300

    feat: added extra attributes to Post entity

--- src/main/java/com/joaquinogallar/personalblog/post/entity/Post.java
+++ src/main/java/com/joaquinogallar/personalblog/post/entity/Post.java
@@ -1,11 +1,16 @@
 package com.joaquinogallar.personalblog.post.entity;
 
+import com.joaquinogallar.personalblog.comment.entity.Comment;
+import com.joaquinogallar.personalblog.tag.entity.Tag;
+import com.joaquinogallar.personalblog.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -16,6 +21,7 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
 
     @Lob
@@ -29,4 +35,18 @@ public class Post {
 
     private Boolean published;
 
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private UserEntity author;
+
+    @ManyToMany
+    @JoinTable(
+            name = "post-tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments;
 }
