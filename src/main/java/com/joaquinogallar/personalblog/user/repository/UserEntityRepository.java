d96df04fe0ff5e14f0dc4fc2989017a61fa850bd
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-17 21:31:49 -0300

    fix: wrong user id type given to the JpaRepository

--- src/main/java/com/joaquinogallar/personalblog/user/repository/UserEntityRepository.java
+++ src/main/java/com/joaquinogallar/personalblog/user/repository/UserEntityRepository.java
@@ -4,6 +4,8 @@ import com.joaquinogallar.personalblog.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+public interface UserEntityRepository extends JpaRepository<UserEntity, UUID> {
 }
