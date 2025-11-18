705630bd7f8203e0529e825e0404437226485a84
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-17 21:12:54 -0300

    feat: create basic methods to get, create, update and delete

--- src/main/java/com/joaquinogallar/personalblog/tag/service/TagService.java
+++ src/main/java/com/joaquinogallar/personalblog/tag/service/TagService.java
@@ -1,8 +1,10 @@
 package com.joaquinogallar.personalblog.tag.service;
 
 import com.joaquinogallar.personalblog.tag.dto.TagDto;
+import com.joaquinogallar.personalblog.tag.entity.Tag;
 import com.joaquinogallar.personalblog.tag.mapper.TagMapper;
 import com.joaquinogallar.personalblog.tag.repository.TagRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,32 +28,50 @@ public class TagService implements ITagService {
 
     @Override
     public TagDto getTagByName(String name) {
-        return null;
+        return tagMapper.mapToTagDto(tagRepository.findByName(name).orElseThrow(() -> new EntityNotFoundException("Tag " + name + " not found")));
     }
 
     @Override
     public TagDto getTagById(Long id) {
-        return null;
+        return tagMapper.mapToTagDto(tagRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Tag " + id + " not found")));
     }
 
     // ------------------------------------------------------------------------------------------------------------------------
     // POST
     @Override
     public TagDto createTag(TagDto tagDto) {
-        return null;
+        Tag tag = Tag.builder()
+                .name(tagDto.name())
+                .slug(tagDto.slug())
+                .build();
+
+        tagRepository.save(tag);
+
+        return tagDto;
     }
 
     // ------------------------------------------------------------------------------------------------------------------------
     // UPDATE
     @Override
     public TagDto updateTag(Long id, TagDto tagDto) {
-        return null;
+        Tag tag =  tagRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Tag " + id + " not found"));
+
+        tag.setName(tagDto.name());
+        tag.setSlug(tagDto.slug());
+
+        tagRepository.save(tag);
+
+        return tagDto;
     }
 
     // ------------------------------------------------------------------------------------------------------------------------
     // DELETE
     @Override
     public TagDto deleteTag(Long id) {
-        return null;
+        Tag tag = tagRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Tag " + id + " not found"));
+
+        tagRepository.delete(tag);
+
+        return tagMapper.mapToTagDto(tag);
     }
 }
