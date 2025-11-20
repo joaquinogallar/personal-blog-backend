3d91e3860c458a0fceb38926ec49142b3858d21b
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-19 21:49:26 -0300

    fix: misstype in Comment Service Interface feat: implement method to comment in a post

--- src/main/java/com/joaquinogallar/personalblog/comment/service/CommentService.java
+++ src/main/java/com/joaquinogallar/personalblog/comment/service/CommentService.java
@@ -1,7 +1,14 @@
 package com.joaquinogallar.personalblog.comment.service;
 
+import com.joaquinogallar.personalblog.comment.dto.CommentDto;
+import com.joaquinogallar.personalblog.comment.entity.Comment;
+import com.joaquinogallar.personalblog.comment.mapper.CommentMapper;
 import com.joaquinogallar.personalblog.comment.repository.CommentRepository;
+import com.joaquinogallar.personalblog.post.entity.Post;
 import com.joaquinogallar.personalblog.post.repository.PostRepository;
+import com.joaquinogallar.personalblog.user.entity.UserEntity;
+import com.joaquinogallar.personalblog.user.repository.UserEntityRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,15 +16,30 @@ public class CommentService implements ICommentService {
 
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
+    private final UserEntityRepository userEntityRepository;
+    private final CommentMapper commentMapper;
 
-    public CommentService(CommentRepository commentRepository, PostRepository postRepository) {
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository, UserEntityRepository userEntityRepository, CommentMapper commentMapper) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
+        this.userEntityRepository = userEntityRepository;
+        this.commentMapper = commentMapper;
     }
 
     @Override
-    public String comment(String comment, Long idPost) {
-        return "";
+    public String comment(CommentDto commentDto, Long idPost) {
+        UserEntity user = userEntityRepository.findById(commentDto.userId()).orElseThrow(() -> new EntityNotFoundException("User " + commentDto.userId() + " not found"));
+        Post post = postRepository.findById(idPost).orElseThrow(() -> new EntityNotFoundException("Post " + idPost + " not found"));
+        Comment comment = Comment.builder()
+                .content(commentDto.content())
+                .user(user)
+                .build();
+
+        post.getComments().add(comment);
+
+        postRepository.save(post);
+
+        return "Comment sent";
     }
 
     @Override
--- src/main/java/com/joaquinogallar/personalblog/comment/service/ICommentService.java
+++ src/main/java/com/joaquinogallar/personalblog/comment/service/ICommentService.java
@@ -1,6 +1,8 @@
 package com.joaquinogallar.personalblog.comment.service;
 
+import com.joaquinogallar.personalblog.comment.dto.CommentDto;
+
 public interface ICommentService {
-    String comment(String comment, Long idPost);
+    String comment(CommentDto comment, Long idPost);
     String deleteComment(Long commentId);
 }
