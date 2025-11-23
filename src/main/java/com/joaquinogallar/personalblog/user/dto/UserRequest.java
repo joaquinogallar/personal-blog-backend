67f28eb2c272aa5e6b704ba5dcfbb2413540e618
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-23 17:18:33 -0300

    feat: added jakarta validation dependency and enhance requestDto

--- src/main/java/com/joaquinogallar/personalblog/user/dto/UserRequest.java
+++ src/main/java/com/joaquinogallar/personalblog/user/dto/UserRequest.java
@@ -1,7 +1,19 @@
 package com.joaquinogallar.personalblog.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
-   String username,
-   String email,
-   String password
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 20)
+        String username,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Bad email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, max = 20)
+        String password
 ) {}
