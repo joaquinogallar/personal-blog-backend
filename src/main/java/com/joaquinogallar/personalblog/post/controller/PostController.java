cd0d3e4f5668113b47f9db46ca7a931e27c7f30b
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-24 17:50:40 -0300

    feat: implemented PostController

--- src/main/java/com/joaquinogallar/personalblog/post/controller/PostController.java
+++ src/main/java/com/joaquinogallar/personalblog/post/controller/PostController.java
@@ -1,10 +1,10 @@
 package com.joaquinogallar.personalblog.post.controller;
 
+import com.joaquinogallar.personalblog.post.dto.CreatePostRequest;
 import com.joaquinogallar.personalblog.post.dto.PostResponse;
 import com.joaquinogallar.personalblog.post.service.IPostService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,4 +18,28 @@ public class PostController {
         this.postService = postService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
+
+    @GetMapping("/{postTitle}")
+    public ResponseEntity<PostResponse> getPostByTitle(@PathVariable String postTitle) {
+        return ResponseEntity.ok(postService.getPostByTitle(postTitle));
+    }
+
+    @PostMapping
+    public ResponseEntity<PostResponse> createPost(@RequestBody CreatePostRequest postReq) {
+        return ResponseEntity.ok(postService.createPost(postReq));
+    }
+
+    @PostMapping("/{idPost}")
+    public ResponseEntity<PostResponse> updatePost(@PathVariable Long idPost, @RequestBody CreatePostRequest postReq) {
+        return ResponseEntity.ok(postService.updatePost(idPost, postReq));
+    }
+
+    @DeleteMapping("/{idPost}")
+    public ResponseEntity<PostResponse> deletePost(@PathVariable Long idPost) {
+        return ResponseEntity.ok(postService.deletePost(idPost));
+    }
 }
