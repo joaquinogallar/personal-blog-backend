d915c703394bf585c518b7c64c95fcd5e3f18732
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-11-24 21:11:42 -0300

    feat: implemented comment controller

--- src/main/java/com/joaquinogallar/personalblog/comment/controller/CommentController.java
+++ src/main/java/com/joaquinogallar/personalblog/comment/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.joaquinogallar.personalblog.comment.controller;
 
+import com.joaquinogallar.personalblog.comment.dto.CreateCommentRequest;
 import com.joaquinogallar.personalblog.comment.service.ICommentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/comments")
@@ -14,5 +17,15 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    @PostMapping("/{postId}/{userId}")
+    public ResponseEntity<String> comment(@PathVariable Long postId,
+                                          @PathVariable UUID userId,
+                                          @RequestBody CreateCommentRequest commentReq) {
+        return ResponseEntity.ok(commentService.comment(commentReq, postId, userId));
+    }
 
+    @DeleteMapping
+    public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
+        return ResponseEntity.ok(commentService.deleteComment(commentId));
+    }
 }
