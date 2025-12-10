3efbb497f486f812d083ac68167ba97cec14f80b
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-12-09 23:24:32 -0300

    feat: added password encoder and hardcoded user

--- src/main/java/com/joaquinogallar/personalblog/config/SecurityConfig.java
+++ src/main/java/com/joaquinogallar/personalblog/config/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.joaquinogallar.personalblog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.joaquinogallar.personalblog.user.entity.Role;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
         .csrf(csrf -> csrf.disable())
@@ -30,4 +40,20 @@ public class SecurityConfig {
         .build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        UserDetails user =
+        User.builder()
+            .username("test")
+            .password(encoder.encode("123456"))
+            .roles(Role.ROLE_ADMIN.toString(), Role.ROLE_USER.toString())
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
