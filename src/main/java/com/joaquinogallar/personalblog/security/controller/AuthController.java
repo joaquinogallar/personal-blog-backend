de31cec0145263cb85a253c4e2dc826ea84e32a4
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-12-30 11:39:54 -0300

    feat: implemented method to login

--- src/main/java/com/joaquinogallar/personalblog/security/controller/AuthController.java
+++ src/main/java/com/joaquinogallar/personalblog/security/controller/AuthController.java
@@ -1,19 +1,48 @@
 package com.joaquinogallar.personalblog.security.controller;
 
+import com.joaquinogallar.personalblog.user.dto.LoginRequest;
+import com.joaquinogallar.personalblog.user.entity.User;
+import com.joaquinogallar.personalblog.user.mapper.UserMapper;
 import com.joaquinogallar.personalblog.user.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository) {
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, UserMapper userMapper) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    loginRequest.usernameOrEmail(),
+                    loginRequest.password()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        User user = userRepository.findUserByUsername(loginRequest.usernameOrEmail())
+                .or(() -> userRepository.findUserByEmail(loginRequest.usernameOrEmail()))
+                .orElseThrow(() -> new UsernameNotFoundException("Username or email doesn't exist"));
+
+
+        return ResponseEntity.ok(userMapper.mapUserToDto(user));
     }
 
 }
