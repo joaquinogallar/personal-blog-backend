c77b10d5cb08f25c300b30f8a4c27553065ecc31
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-12-30 11:38:53 -0300

    feat: implemented AuthenticationManager Bean

--- src/main/java/com/joaquinogallar/personalblog/security/config/SecurityConfig.java
+++ src/main/java/com/joaquinogallar/personalblog/security/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.joaquinogallar.personalblog.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
@@ -52,6 +54,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
