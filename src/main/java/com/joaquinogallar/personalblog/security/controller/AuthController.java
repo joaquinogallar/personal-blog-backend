cd70a598fedb5156901ded3023ed08a3efb33b0b
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-12-30 11:26:19 -0300

    feat: created AuthController to implement login logic

---
+++ src/main/java/com/joaquinogallar/personalblog/security/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.joaquinogallar.personalblog.security.controller;
+
+import com.joaquinogallar.personalblog.user.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+    }
+
+}
