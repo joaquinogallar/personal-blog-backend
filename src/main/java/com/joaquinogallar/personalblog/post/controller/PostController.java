7d862ca8e672e57c31f641ff533493c4e51e3201
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-02-20 21:05:21 -0300

    feat: improve status code handling

--- src/main/java/com/joaquinogallar/personalblog/post/controller/PostController.java
+++ src/main/java/com/joaquinogallar/personalblog/post/controller/PostController.java
@@ -6,6 +6,7 @@ import com.joaquinogallar.personalblog.post.service.IPostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,16 +32,22 @@ public class PostController {
 
     @PostMapping
     public ResponseEntity<PostResponse> createPost(@RequestBody CreatePostRequest postReq) {
-        return ResponseEntity.ok(postService.createPost(postReq, "testUser")); // hadrcoded username for testing
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(postService.createPost(postReq, "testUser")); // hadrcoded username for testing
     }
 
     @PostMapping("/{idPost}")
     public ResponseEntity<PostResponse> updatePost(@PathVariable Long idPost, @RequestBody CreatePostRequest postReq) {
-        return ResponseEntity.ok(postService.updatePost(idPost, postReq));
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(postService.updatePost(idPost, postReq));
     }
 
     @DeleteMapping("/{idPost}")
     public ResponseEntity<PostResponse> deletePost(@PathVariable Long idPost) {
-        return ResponseEntity.ok(postService.deletePost(idPost));
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(postService.deletePost(idPost));
     }
 }
