f64d670c786d8153f5a878e87c4e4afce8604082
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-02-23 19:42:49 -0300

    feat: add register user method

--- src/main/java/com/joaquinogallar/personalblog/security/controller/AuthController.java
+++ src/main/java/com/joaquinogallar/personalblog/security/controller/AuthController.java
@@ -1,17 +1,23 @@
 package com.joaquinogallar.personalblog.security.controller;
 
 import com.joaquinogallar.personalblog.user.dto.LoginRequest;
+import com.joaquinogallar.personalblog.user.dto.UserRequest;
 import com.joaquinogallar.personalblog.user.dto.UserResponse;
+import com.joaquinogallar.personalblog.user.entity.Role;
 import com.joaquinogallar.personalblog.user.entity.User;
 import com.joaquinogallar.personalblog.user.mapper.UserMapper;
 import com.joaquinogallar.personalblog.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -19,11 +25,13 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, UserMapper userMapper) {
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping()
@@ -44,6 +52,26 @@ public class AuthController {
 
 
         return ResponseEntity.ok(userMapper.mapUserToDto(user));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest request) {
+
+        if (userRepository.existsByUsername(request.username())) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = User.builder()
+                .username(request.username())
+                .email(request.email())
+                .passwordHash(passwordEncoder.encode(request.password()))
+                .isActive(true)
+                .roles(Set.of(Role.USER))
+                .build();
+
+        userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.mapUserToDto(user));
     }
 
     @GetMapping("/me")
