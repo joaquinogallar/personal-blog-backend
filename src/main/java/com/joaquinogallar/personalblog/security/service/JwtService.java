3b467c60d70ed47aa5b6251b4e28bcc1347839cc
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-02-23 20:08:01 -0300

    feat: add methods to generate jwt token and extract a username from a token

--- src/main/java/com/joaquinogallar/personalblog/security/service/JwtService.java
+++ src/main/java/com/joaquinogallar/personalblog/security/service/JwtService.java
@@ -1,10 +1,34 @@
 package com.joaquinogallar.personalblog.security.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtService {
 
+    @Value("${app.jwt.secret}")
+    private String SECRET;
 
+    public String generateToken(UserDetails user) {
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+    }
 
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
 }
